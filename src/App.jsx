1ccbae0c7081e5ada7add89e17c8e914989b69c8
rouanis5 <ouanis42@gmail.com>
2022-10-22 10:44:54 +0100

    finish step 6

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,23 @@
-import { useState } from 'react'
+import axios from 'axios'
+import { useState, useEffect } from 'react'
 import Filter from './Components/Filter'
 import PersonForm from './Components/PersonForm'
 import Persons from './Components/Persons'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
 
   const [newFilter, setNewFilter] = useState('')
+
+  useEffect(()=>{
+    axios.get("http://localhost:3001/persons")
+    .then(res => res.data)
+    .then(data => {
+      setPersons(data)
+    })
+  },[])
 
   function SubmitNewPerson(event){
     //prevent default action
