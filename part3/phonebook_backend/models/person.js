051d9bb80b87e5a58b93cb9dfd675b4ee9a1359c
rouanis5 <ouanis42@gmail.com>
2022-12-26 21:35:14 +0100

    + personSchema validators 4 name & number

--- part3/phonebook_backend/models/person.js
+++ part3/phonebook_backend/models/person.js
@@ -16,11 +16,21 @@ mongoose.connect(url)
 const personSchema = new mongoose.Schema({
   name: {
     type: String,
+    minLength: 3,
     required: true
   },
   number: {
     type: String,
-    minLength: 3,
+    validate: {
+      validator: function(v) {
+        // has length of 8 or more
+        // if formed of two parts that are separated by -
+        // the first part has two or three numbers 
+        // and the second part also consists of numbers
+        return /(^\d{2}-{0,1}\d{6,}$)|(^\d{3}-{0,1}\d{5,}$)/.test(v);
+      },
+      message: ({value}) => `${value} is not a valid phone number!`
+    },
     required: true
   },
 })
