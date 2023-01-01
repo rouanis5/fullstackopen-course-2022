ff5b2468dd61d7840cc712f6664fa5971444e2e5
rouanis5 <ouanis42@gmail.com>
2023-01-01 21:47:04 +0100

    step5: test most publishing & liked author (hard)

--- part4/blog_list/utils/list_helper.js
+++ part4/blog_list/utils/list_helper.js
@@ -23,8 +23,76 @@ const favoriteBlog = (blogs) => {
   })
 }
 
+const mostBlogs = (blogs) => {
+  if (blogs.length === 0) return {} // empty blogs
+  // if only one, just return it :)
+  if (blogs.length === 1) return { author: blogs[0].author, blogs: 1 }
+
+  // this function initialize an empty object
+  // while looping the blogs, the func will initialize
+  // a key with the name of the author with a value of 1
+  // while looping, everytime he appear. it will increase its value by 1
+  // expected result:
+  // {
+  //   'aaa hehe': 1,
+  //   'zzz jj': 2,
+  //   ...
+  // }
+  let authors = blogs.reduce((obj, blog) => {
+    if (!obj[blog.author]) {
+      obj[blog.author] = 1
+    } else {
+      obj[blog.author]++
+    }
+    return obj
+  }, {})
+
+  // Object.keys(authors) return an array that contains all authors
+  // [ {author: 'aa', blogs: 1}, {author: 'bb', blogs: 1} ..... ]
+  authors = Object.keys(authors).map(name => {
+    return {
+      author: name,
+      blogs: authors[name]
+    }
+  })
+
+  // return the author who has the biggest num of blogs
+  return authors.reduce((obj, author, index) => {
+    if (index === 0 | author.blogs > obj.blogs) return author
+    return obj
+  })
+}
+
+const mostLikes = (blogs) => {
+  // the logic is the same as the previous function
+  if (blogs.length === 0) return {}
+  if (blogs.length === 1) return { author: blogs[0].author, likes: blogs[0].likes }
+
+  let authors = blogs.reduce((obj, blog) => {
+    if (!obj[blog.author]) {
+      obj[blog.author] = 0 // initialize
+    }
+    obj[blog.author] += blog.likes
+    return obj
+  }, {})
+
+  authors = Object.keys(authors).map(name => {
+    return {
+      author: name,
+      likes: authors[name]
+    }
+  })
+
+  return authors.reduce((obj, author, index) => {
+    if (index === 0 | author.likes > obj.likes) return author
+    return obj
+  })
+}
+
 module.exports = {
   dummy,
   totalLikes,
-  favoriteBlog
+  favoriteBlog,
+  mostBlogs,
+  mostLikes
 }
