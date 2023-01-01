d40964b4a5a07810a0cc7946fc8d2e14e2c582b3
rouanis5 <ouanis42@gmail.com>
2023-01-01 20:26:13 +0100

    step3: + favoriteBlog test, use reduce method

--- part4/blog_list/utils/list_helper.js
+++ part4/blog_list/utils/list_helper.js
@@ -3,14 +3,28 @@ const dummy = (blogs) => {
 }
 
 const totalLikes = (blogs) => {
-  let sum = 0
-  blogs.forEach(blog => {
-    sum += blog.likes
+  // old syntax
+  // let sum = 0
+  // blogs.forEach(blog => {
+  //   sum += blog.likes
+  // })
+  // return sum
+
+  // new syntax with reduce
+  return blogs.reduce((sum, blog) => {
+    return sum + blog.likes
+  }, 0)
+}
+
+const favoriteBlog = (blogs) => {
+  return blogs.reduce((favorite, blog, index) => {
+    if (index === 0 || blog.likes > favorite.likes) return blog
+    return favorite
   })
-  return sum
 }
 
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
