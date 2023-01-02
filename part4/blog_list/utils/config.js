7d42a16c96372cb5d1ce9611b5be8e24347c6a69
rouanis5 <ouanis42@gmail.com>
2023-01-02 23:11:09 +0100

    preparing backend for testing

--- part4/blog_list/utils/config.js
+++ part4/blog_list/utils/config.js
@@ -1,7 +1,9 @@
 require('dotenv').config()
 
 const PORT = process.env.PORT
-const MONGO_URL = process.env.MONGO_URL
+const MONGO_URL = process.env.NODE_ENV === 'test'
+  ? process.env.TEST_MONGODB_URI
+  : process.env.MONGO_URL
 
 module.exports = {
   PORT,
