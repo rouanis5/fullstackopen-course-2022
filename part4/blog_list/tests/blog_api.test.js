15c69bbed2d995dc484b8c7b2b3f04046d2aa7a9
rouanis5 <ouanis42@gmail.com>
2023-01-03 18:36:14 +0100

    Blog list tests, step3
    verify that a blog added successfully and the number of blogs is increased by one

--- part4/blog_list/tests/blog_api.test.js
+++ part4/blog_list/tests/blog_api.test.js
@@ -20,11 +20,28 @@ test('returns the correct amount of blog posts in the JSON format', async () => 
   expect(response.body).toHaveLength(helper.initialBlogs.length)
 })
 
-test('verifies that the unique identifier property of the blog posts is named id', async () => {
+test('verifiy that the unique identifier property of the blog posts is named id', async () => {
   const response = await api.get('/api/blogs')
   const firstBlog = response.body[0]
   expect(firstBlog._id).not.toBeDefined()
   expect(firstBlog.id).toBeDefined()
+})
+
+test('verify that a blog added successfully and the number of blogs is increased by one', async () => {
+  const oneBlog = {
+    title: 'TDD harms architecture',
+    author: 'Robert C. Martin',
+    url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
+    likes: 0
+  }
+  const response1 = await api.post('/api/blogs')
+    .send(oneBlog)
+    .expect(201)
+  expect(response1.body.url).toBe(oneBlog.url)
+
+  const response2 = await api.get('/api/blogs')
+    .expect(200)
+  expect(response2.body).toHaveLength(++helper.initialBlogs.length)
 })
 
 afterAll(() => {
