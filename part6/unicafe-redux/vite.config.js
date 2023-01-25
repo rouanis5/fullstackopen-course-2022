b615cb12293f9191800d3ea885659010064963ac
rouanis5 <ouanis42@gmail.com>
2023-01-25 21:19:46 +0100

    6.unicafe-redux project init files

---
+++ part6/unicafe-redux/vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vitest/config'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  // server: {
+  //   proxy: {
+  //     '/api/': 'http://localhost:3001/'
+  //   }
+  // },
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    watch: false
+  },
+})
